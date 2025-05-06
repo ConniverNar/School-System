393d28c76ff40a13928e97058d30df81d50efd15
OmoCarl1 <23102747@usc.edu.ph>
2025-05-06 22:23:06 +0800

    Add files via upload
    Signed-off-by: OmoCarl1 <23102747@usc.edu.ph>

--- Subject.java
+++ Subject.java
@@ -39,8 +39,16 @@ public class Subject {
         return name;
     }
     
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public String getDepartment() {
         return department;
+    }
+    
+    public void setDepartment(String department) {
+        this.department = department;
     }
     
     public double getTuition() {
