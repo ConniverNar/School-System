3fa11ed680e5e5ec138f056619033dcefaeb7938
Kurt Casey V. Naranjo <63389327+ConniverNar@users.noreply.github.com>
2025-05-11 23:45:12 +0800

    Update DatabaseManager.java
    added preloaded 2 faculty users
    
    Signed-off-by: Kurt Casey V. Naranjo <63389327+ConniverNar@users.noreply.github.com>

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -46,10 +46,24 @@ public class DatabaseManager {
         student3.setUserInfo("department", "Computer Engineering");
         student3.setUserInfo("schoolYear", "2025");
 
+        User faculty1 = new User("Steve", "123", User.UserType.FACULTY);
+        faculty1.setUserInfo("name", "Steve Rogers");
+        faculty1.setUserInfo("gender", "Male");
+        faculty1.setUserInfo("age", "39");
+        faculty1.setUserInfo("department", "Computer Engineering");
+
+        User faculty2 = new User("Stephanie", "123", User.UserType.FACULTY);
+        faculty2.setUserInfo("name", "Stephanie Shane");
+        faculty2.setUserInfo("gender", "Female");
+        faculty2.setUserInfo("age", "32");
+        faculty2.setUserInfo("department", "Computer Engineering");
+
         users.put(admin.getUsername(), admin);
         users.put(student1.getUsername(), student1);
         users.put(student2.getUsername(), student2);
         users.put(student3.getUsername(), student3);
+        users.put(faculty1.getUsername(), faculty1);
+        users.put(faculty2.getUsername(), faculty2);
     }
 
     // Authenticate a user based on credentials
